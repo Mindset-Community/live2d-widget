90ddeb73f51d3d36ef01ca7e254f08aee93b6337
Jonas-rbg <jonashaavisto@gmail.com>
2024-05-16 12:51:00 +0300

    test
    test

--- autoload.js
+++ autoload.js
@@ -1,6 +1,6 @@
 // live2d_path 参数建议使用绝对路径
 const live2d_path =
-  "https://fastly.jsdelivr.net/gh/Mindset-Community/live2d-widget@0.2.3/";
+  "https://fastly.jsdelivr.net/gh/Mindset-Community/live2d-widget@master/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
@@ -36,7 +36,7 @@ if (screen.width >= 768) {
       waifuPath: live2d_path + "waifu-tips.json",
       //apiPath: "https://live2d.fghrsh.net/api/",
       cdnPath:
-        "https://fastly.jsdelivr.net/gh/Mindset-Community/live2d_api@0.4.2/",
+        "https://fastly.jsdelivr.net/gh/Mindset-Community/live2d_api@0.4.3/",
       tools: ["asteroids", "photo", "quit"],
       //  "switch-model","switch-texture","info","hitokoto",
     });
